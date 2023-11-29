5efd5c7436e92a66cb4b1c416358070601676285
AndroidHQ254 <d.murimi14@gmail.com>
2023-11-30 01:07:56 +0300

    toco: Adapt to PixelOS
    Co-Authored-By: aryan. <66957409+basamaryan@users.noreply.github.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_toco.mk
+    $(LOCAL_DIR)/aosp_toco.mk
--- device.mk
+++ device.mk
@@ -57,11 +57,15 @@ PRODUCT_COPY_FILES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-custom
 
 # Power
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePkgs
 
 # Sensors
 PRODUCT_PACKAGES += \

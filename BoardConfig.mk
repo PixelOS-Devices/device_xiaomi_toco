bd9dab2f01b7f07051929c4b7129cb7fb9916761
AndroidHQ254 <d.murimi14@gmail.com>
2024-05-23 12:46:21 +0300

    toco: Drop target verified boot

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,12 +45,5 @@ TARGET_USES_FOD_ZPOS := true
 # Vendor security patch level
 VENDOR_SECURITY_PATCH := 2022-10-01
 
-# Verified boot
-BOARD_AVB_VBMETA_SYSTEM := product system system_ext
-BOARD_AVB_VBMETA_SYSTEM_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
-BOARD_AVB_VBMETA_SYSTEM_ALGORITHM := SHA256_RSA2048
-BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
-BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX_LOCATION := 1
-
 # Inherit from proprietary files
 include vendor/xiaomi/toco/BoardConfigVendor.mk

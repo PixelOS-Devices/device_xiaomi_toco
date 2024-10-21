44318e889261e8ddb7f4db6643cdb389d0a3cb84
Dennis Murimi <d.murimi14@gmail.com>
2024-10-21 16:07:24 +0300

    toco: Patch `libalRnBRT_GL_GBWRAPPER.so` for missing GraphicBufferMapper symbol
    out/soong/.intermediates/vendor/xiaomi/toco/libalRnBRT_GL_GBWRAPPER/android_vendor_arm64_armv8-2a-dotprod_cortex-a55_shared/fafa22850919164bce28a6fe4914a49a/libalRnBRT_GL_GBWRAPPER.so: error: Unresolved symbol: _ZN7android13GraphicBuffer4lockEjPPv

--- extract-files.sh
+++ extract-files.sh
@@ -25,6 +25,10 @@ function blob_fixup() {
             sed -i "s|libMegviiFacepp-0.5.2.so|libFaceDetectpp-0.5.2.so|g" "${2}"
             sed -i "s|megviifacepp_0_5_2_model|facedetectpp_0_5_2_model|g" "${2}"
             ;;
+        vendor/lib64/libalRnBRT_GL_GBWRAPPER.so)
+            [ "$2" = "" ] && return 0
+            grep -q "libui_shim.so" "${2}" || ${PATCHELF} --add-needed "libui_shim.so" "${2}"
+            ;;
         *)
             return 1
             ;;
